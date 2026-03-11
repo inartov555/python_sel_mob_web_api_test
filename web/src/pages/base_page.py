f18056167ec5a5b018cb54cc6075e5db3d02ed87
Iurii Nartov <yurahttp@gmail.com>
2026-03-11 13:37:59 +0200

    E0611: No name 'ActionChains' in module 'selenium.webdriver' (no-name-in-module)

--- web/src/pages/base_page.py
+++ web/src/pages/base_page.py
@@ -6,7 +6,7 @@ import time
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
 from tools.logger.logger import Logger
